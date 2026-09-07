448225636621030f74368628233e0c70d69b481b
stylishdubaiibag-boop <stylishdubaiibag@gmail.com>
2026-09-07 23:03:02 +0500

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p ~/.vnc
-echo "password" | vncpasswd -f > ~/.vnc/passwd
-chmod 600 ~/.vnc/passwd
-vncserver :1 -geometry 1280x800 -depth 24
-websockify --web /usr/share/novnc/ 8080 localhost:5901
